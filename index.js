afe444eb949d7b191029f7150a8acad3a3a7e775
Louis Dierickx <dierickx.louis01@gmail.com>
2024-02-15 19:28:31 +0100

    update bckcolor

--- index.js
+++ index.js
@@ -9,40 +9,11 @@ const json = async () => {
 json()
 .then((data) => {
     body.innerHTML = `
+    <div class="card">
         <h1>${data.title}</h1>
+        <img src="${data.img}">
         <h3>${data.info}</h3>
-        <p>${data.hobby}</p>
+        <p>${data.tchn}</p>
+    </div>
     `
 });
-
-// const body = document.querySelector('body');
-
-// const getJson = async () => {
-//     try {
-//         const response = await fetch("index.json");
-
-//         if (!response.ok) {
-//             throw new Error(`Error: ${response.status}`);
-//         }
-
-//         const data = await response.json();
-//         return data;
-
-//     } catch (error) {
-//         console.error(`Error fetching data: ${error.message}`);
-//         throw error; // Re-throw the error to be caught in the outer catch block if needed
-//     }
-// };
-
-// getJson()
-//     .then((data) => {
-//         body.innerHTML = `
-//             <h1>${data.title}</h1>
-//             <h3>${data.info}</h3>
-//             <p>${data.hobby}</p>
-//         `;
-//     })
-//     .catch((error) => {
-//         console.error(`Error in promise chain: ${error.message}`);
-//     });
-
